2c2ce6668544b848d783e754ba898528afce72a5
Sarah Kolb <sarah.n.kolb@gmail.com>
2023-06-02 14:23:59 +0200

    Projektdateien hinzufügen.

---
+++ Bio-Store/LoadingScreen.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Bio_Store
+{
+    partial class LoadingScreen
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblProgressValue = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.progressBarTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Bio_Store.Properties.Resources.GroceryImg;
+            this.pictureBox1.Location = new System.Drawing.Point(0, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(802, 262);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Snow;
+            this.label1.Location = new System.Drawing.Point(346, 275);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Loading";
+            // 
+            // lblProgressValue
+            // 
+            this.lblProgressValue.AutoSize = true;
+            this.lblProgressValue.ForeColor = System.Drawing.Color.Snow;
+            this.lblProgressValue.Location = new System.Drawing.Point(418, 275);
+            this.lblProgressValue.Name = "lblProgressValue";
+            this.lblProgressValue.Size = new System.Drawing.Size(32, 20);
+            this.lblProgressValue.TabIndex = 2;
+            this.lblProgressValue.Text = "0%";
+            // 
+            // progressBar
+            // 
+            this.progressBar.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.progressBar.Location = new System.Drawing.Point(29, 301);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(745, 31);
+            this.progressBar.TabIndex = 3;
+            // 
+            // progressBarTimer
+            // 
+            this.progressBarTimer.Interval = 50;
+            this.progressBarTimer.Tick += new System.EventHandler(this.progressBarTimer_Tick);
+            // 
+            // LoadingScreen
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(798, 349);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.lblProgressValue);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LoadingScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Loading Screen";
+            this.Load += new System.EventHandler(this.LoadingScreen_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblProgressValue;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Timer progressBarTimer;
+    }
+}
+
---
+++ Bio-Store/LoadingScreen.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bio_Store
+{
+    public partial class LoadingScreen : Form
+    {
+        private int loadingBarValue; 
+        public LoadingScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadingScreen_Load(object sender, EventArgs e)
+        {
+            progressBarTimer.Start(); 
+        }
+
+        private void progressBarTimer_Tick(object sender, EventArgs e)
+        {
+            loadingBarValue += 1;
+
+            progressBar.Value = loadingBarValue;
+            lblProgressValue.Text = loadingBarValue.ToString() + "%"; 
+
+            if (loadingBarValue >= progressBar.Maximum)
+            {
+                progressBarTimer.Stop(); 
+            }
+
+        }
+    }
+}
